99db7ab7a87d466dca9d45903f09fb2e761543cc
rajeshdakka <dakkarajesh18@gmail.com>
2025-09-12 11:26:28 +0530

    updated code with changes

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
